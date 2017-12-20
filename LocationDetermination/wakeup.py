1176e2aa6679bdc962b1d0802715e609f21c4332
Unknown <robbe.elsas@gmail.com>
2017-12-20 16:24:00 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 2cb9406c5460718d6df6e73cdc21634851870693.

--- LocationDetermination/wakeup.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-url = "http://thingsboard.idlab.uantwerpen.be:8080/api/v1/66050360-b8b7-11e7-bebc-85e6dd10a2e8/attributes"
-data = {'budgetA': 1, 'budgetB':1,
-        'budgetC': 1, 'budgetD': 1}
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-r = requests.post(url, data=json.dumps(data), headers=headers);
