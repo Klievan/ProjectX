ceaa4d7dfd18e7f4d3a137bdd525a4a72fb2341f
Unknown <robbe.elsas@gmail.com>
2018-01-09 13:02:51 +0100

    Dash7 TX added

---
+++ Final/Inc/constants.h
@@ -0,0 +1,132 @@
+/*
+ * constants.h
+ *
+ *  Created on: 4 dec. 2017
+ *      Author: Robbe
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+// I2C slave addresses
+#define LSM303_ACC_ADDR 		0x32
+#define LSM303_MAG_ADDR 		0x3C
+#define LPS22HB_ADDR 			0xBA
+
+// LPS22HB register addresses
+#define LPS22HB_CTRL_REG1 		0x10
+
+// LPS22HB register settings
+#define LPS22HB_ODR_1HZ			0x10
+#define LPS22HB_ODR_10HZ		0x20
+#define LPS22HB_ODR_25HZ		0x30
+#define LPS22HB_ODR_50HZ		0x40
+#define LPS22HB_ODR_75HZ		0x50
+
+// LSM303AGR ACC register addresses
+#define LSM303_ACC_CTRL_REG1 	0x20
+#define LSM303_ACC_CTRL_REG2 	0x21
+#define LSM303_ACC_CTRL_REG3 	0x22
+#define LSM303_ACC_CTRL_REG4 	0x23
+#define LSM303_ACC_CTRL_REG5 	0x24
+#define LSM303_ACC_CTRL_REG6 	0x25
+#define LSM303_ACC_SREG 		0x27
+#define LSM303_ACC_X_L 			0x28
+#define LSM303_ACC_X_H 			0x29
+#define LSM303_ACC_Y_L 			0x2A
+#define LSM303_ACC_Y_H 			0x2B
+#define LSM303_ACC_Z_L 			0x2C
+#define LSM303_ACC_Z_H 			0x2D
+
+// LSM303AGR ACC register settings
+#define LSM303_ACC_PWR_DWN		0x00
+#define LSM303_ACC_ODR_1HZ		0x10
+#define LSM303_ACC_ODR_10HZ		0x20
+#define LSM303_ACC_ODR_25HZ		0x30
+#define LSM303_ACC_ODR_50HZ		0x40
+#define LSM303_ACC_ODR_100HZ	0x50
+#define LSM303_ACC_LPEN			0x08
+#define LSM303_ACC_XEN			0x01
+#define LSM303_ACC_YEN			0x02
+#define LSM303_ACC_ZEN			0x04
+#define LSM303_ACC_XYZEN		0x07
+#define LSM303_ACC_I1_DRDY2		0x08
+#define LSM303_ACC_BDU			0x10
+#define LSM303_ACC_FS_2G		0x00
+#define LSM303_ACC_FS_4G		0x10
+#define LSM303_ACC_FS_8G		0x20
+#define LSM303_ACC_FS_16G		0x30
+#define LSM303_ACC_BOOT			0x80
+
+// LSM303AGR ACC register values
+#define LSM303_ACC_XYZDA		0x04
+
+// LSM303AGR MAG register addresses
+#define LSM303_MAG_CFG_REGA 	0x60
+#define LSM303_MAG_CFG_REGB 	0x61
+#define LSM303_MAG_CFG_REGC 	0x62
+#define LSM303_MAG_SREG			0x67
+#define LSM303_MAG_X_L 			0x68
+#define LSM303_MAG_X_H 			0x69
+#define LSM303_MAG_Y_L 			0x6A
+#define LSM303_MAG_Y_H 			0x6B
+#define LSM303_MAG_Z_L 			0x6C
+#define LSM303_MAG_Z_H 			0x6D
+
+// LSM303AGR MAG register settings
+#define LSM303_MAG_ODR_10HZ		0x00
+#define LSM303_MAG_ODR_20HZ		0x04
+#define LSM303_MAG_ODR_50HZ		0x08
+#define LSM303_MAG_ODR_100HZ	0x0C
+#define LSM303_MAG_REBOOT		0x40
+#define LSM303_MAG_SOFT_RST		0x20
+#define LSM303_MAG_LPEN			0x10
+#define LSM303_MAG_MD_CON		0x00
+#define LSM303_MAG_MD_SIN		0x01
+#define LSM303_MAG_MD_IDLE		0x03
+#define LSM303_MAG_BDU			0x10
+#define LSM303_MAG_INT			0x01
+#define LSM303_MAG_OFF_CANC		0x02
+#define LSM303_MAG_LPF			0x01
+
+// LSM303AGR MAG register values
+#define LSM303_MAG_XYZDA		0x04
+
+// LSM303AGR Axes
+#define X 0
+#define Y 1
+#define Z 2
+
+const uint8_t MESSAGE_MAX_LEN	= 0xFF;
+const uint8_t FLOOD_TAIL[] 		= {0x6B,0x61,0x6B,0x61};
+const uint8_t TAIL_SEPARATOR 	= 0x3B;
+
+const uint32_t LSM303_ACC_SENSITIVITY[3][4] = {
+		/* HR 12-bit */
+		{
+				980, /* FS @2g */
+				1950, /* FS @4g */
+				3900, /* FS @8g */
+				11720, /* FS @16g */
+		},
+		/* Normal 10-bit */
+		{
+				3900, /* FS @2g */
+				7820, /* FS @4g */
+				15630, /* FS @8g */
+				46900, /* FS @16g */
+		},
+		/* LP 8-bit */
+		{
+				15630, /* FS @2g */
+				31260, /* FS @4g */
+				62520, /* FS @8g */
+				187580, /* FS @16g */
+		},};
+
+const uint8_t UNSOLICITED_RESPONSE_BASE[] = {0x41,0x54,0x24,0x44,0xC0,0x00, // use serial interface
+											 0x00, // command length = number of trailing bytes including data = dummy
+											 0xB4,0x13,0x32,0xD7,0x00,0x00,0x10,0x01, // indirect forward + operand
+											 0x20,0x40,0x00}; // return file data action with fileID = 40 and offset = 0
+
+#endif /* CONSTANTS_H_ */
--- Final/Src/main.c
+++ Final/Src/main.c
@@ -41,7 +41,15 @@
 #include "stm32l1xx_hal.h"
 
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "enumerations.h"
+#include "constants.h"
+#include "structures.h"
+#include "macros.h"
+// #define NSERDEBUG // comment to turn on serial debug over UART2
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
@@ -57,7 +65,8 @@ DMA_HandleTypeDef hdma_usart1_rx;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
+static volatile uint8_t TRANSMITTING = 0;
+static int32_t hardIronFault[] = {0, 0, 0};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -73,7 +82,21 @@ static void MX_RTC_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
-
+static int64_t twosComplementToSignedInteger(uint32_t rawValue,SignBitIndex sbi);
+static uint8_t unsolicitedResponseTX(uint8_t* data, uint8_t dataLength);
+static UnsolicitedResponseTail buildTail(UnsolicitedResponseType urt);
+static UnsolicitedResponseTail mergeTails(UnsolicitedResponseTail firstTail,UnsolicitedResponseTail secondTail);
+static CompassData getCompassData();
+static BarometerData getBarometerData();
+static MagnetometerData getMagnetometerData();
+static AccelerometerData getAccelerometerData();
+static void get_m_axes(int32_t *pData);
+static void get_m_axes_raw(int16_t *pData);
+static void get_a_axes(int32_t *pData);
+static void get_a_axes_raw(int16_t *pData);
+static void enterStopMode(uint32_t regulator);
+static void enterSleepMode(uint32_t regulator);
+static void getHardIronFault();
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
@@ -114,7 +137,45 @@ int main(void)
   MX_RTC_Init();
 
   /* USER CODE BEGIN 2 */
+  HAL_PWREx_EnableLowPowerRunMode();
 
+  uint8_t REG_VALUE = LPS22HB_ODR_1HZ;
+  HAL_I2C_Mem_Write(&hi2c1,LPS22HB_ADDR,LPS22HB_CTRL_REG1,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while(HAL_I2C_IsDeviceReady(&hi2c1,LPS22HB_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  REG_VALUE = LSM303_MAG_SOFT_RST | LSM303_MAG_LPEN | LSM303_MAG_ODR_10HZ | LSM303_MAG_MD_IDLE;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_CFG_REGA,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_MAG_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  REG_VALUE = LSM303_MAG_LPF;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_CFG_REGB,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_MAG_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  REG_VALUE = LSM303_MAG_BDU;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_CFG_REGC,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_MAG_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  HAL_Delay(10); // ACC startup time < 5ms
+
+  REG_VALUE = LSM303_ACC_PWR_DWN | LSM303_ACC_XYZEN | LSM303_ACC_LPEN;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_CTRL_REG1,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_ACC_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  REG_VALUE = LSM303_ACC_I1_DRDY2;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_CTRL_REG3,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_ACC_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  REG_VALUE = LSM303_ACC_BDU | LSM303_ACC_FS_2G;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_CTRL_REG4,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_ACC_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  REG_VALUE = LSM303_ACC_BOOT;
+  HAL_I2C_Mem_Write(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_CTRL_REG5,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+  while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_ACC_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+  HAL_Delay(10); // wait for changes to take effect
+
+  getHardIronFault();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -124,7 +185,17 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
-
+	  enterSleepMode(PWR_LOWPOWERREGULATOR_ON);
+	  HAL_PWREx_EnableLowPowerRunMode();
+	  if (TRANSMITTING) {
+		  UnsolicitedResponseTail firstTail = buildTail(BAROMETER_FRAME);
+		  UnsolicitedResponseTail secondTail = buildTail(MAGNETOMETER_FRAME);
+		  UnsolicitedResponseTail thirdTail = buildTail(ACCELEROMETER_FRAME);
+		  UnsolicitedResponseTail fourthTail = buildTail(COMPASS_FRAME);
+		  UnsolicitedResponseTail tail = mergeTails(mergeTails(firstTail,secondTail),mergeTails(thirdTail,fourthTail));
+		  unsolicitedResponseTX(tail.data,tail.dataLength);
+		  TRANSMITTING = 0;
+	  }
   }
   /* USER CODE END 3 */
 
@@ -411,7 +482,384 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+static void getHardIronFault() {
+	int32_t mag_temp[3] = {0, 0, 0};
+	int32_t mag_max[3] = {-2147483647, -2147483647, -2147483647};
+	int32_t mag_min[3] = {2147483647, 2147483647, 2147483647};
 
+	// start calibration by pressing s key
+	uint8_t temp[1];
+	do
+		HAL_UART_Receive(&huart2,temp,1,HAL_MAX_DELAY);
+	while (*temp != 0x73);
+
+	char message[255];
+	sprintf(message, "Calibration started.\r\n");
+	HAL_UART_Transmit(&huart2, (uint8_t *) message, strlen(message), HAL_MAX_DELAY);
+
+	for(uint16_t i = 0; i < 300; ++i) {
+		get_m_axes(mag_temp);
+		for (int j = 0; j < 3; ++j) {
+			if(mag_temp[j] > mag_max[j]) mag_max[j] = mag_temp[j];
+			if(mag_temp[j] < mag_min[j]) mag_min[j] = mag_temp[j];
+		}
+		HAL_Delay(100);
+	}
+
+	hardIronFault[X] = (mag_max[X] + mag_min[X]) / 2;
+	hardIronFault[Y] = (mag_max[Y] + mag_min[Y]) / 2;
+	hardIronFault[Z] = (mag_max[Z] + mag_min[Z]) / 2;
+
+	sprintf(message, "Calibration is done!\r\n");
+	HAL_UART_Transmit(&huart2, (uint8_t *) message, strlen(message), HAL_MAX_DELAY);
+
+#ifdef NSERDEBUG
+	HAL_UART_DeInit(&huart2);
+#endif
+}
+
+static uint8_t unsolicitedResponseTX(uint8_t* data, uint8_t dataLength) {
+	if ((dataLength + 1 + sizeof(UNSOLICITED_RESPONSE_BASE)) <= MESSAGE_MAX_LEN) {
+		// Memory allocated with malloc never loses scope until forcefully freed or program termination occurs.
+		uint8_t* unsollicitedResponse = (uint8_t*) malloc((dataLength + 1 + sizeof(UNSOLICITED_RESPONSE_BASE)) * sizeof(uint8_t));
+
+		for (uint8_t i = 0; i < sizeof(UNSOLICITED_RESPONSE_BASE); ++i)
+			unsollicitedResponse[i] = ((i == 6) ? (0x0B + 1 + dataLength) : (UNSOLICITED_RESPONSE_BASE[i]));
+
+		unsollicitedResponse[sizeof(UNSOLICITED_RESPONSE_BASE)] = dataLength;
+
+		for (uint8_t i = 0; i < dataLength; ++i)
+			unsollicitedResponse[sizeof(UNSOLICITED_RESPONSE_BASE) + 1 + i] = data[i];
+		free(data);
+
+		HAL_UART_Transmit(&huart4, unsollicitedResponse, dataLength + 1 + sizeof(UNSOLICITED_RESPONSE_BASE),HAL_MAX_DELAY);
+		free(unsollicitedResponse);
+
+		return 1;
+	}
+	return 0;
+}
+
+static UnsolicitedResponseTail buildTail(UnsolicitedResponseType urt) {
+	/*
+	 * Memory allocated with malloc never loses scope until it is forcefully freed using
+	 * the free function or when the program terminates. Therefore a pointer to this memory
+	 * location is passed as a return value so it can be freed later (after use).
+	 */
+	uint8_t* data = (uint8_t*) malloc((MESSAGE_MAX_LEN - sizeof(UNSOLICITED_RESPONSE_BASE)) * sizeof(uint8_t));
+	uint8_t dataLength = 0;
+	UnsolicitedResponseTail tail = {data,dataLength};
+	int32_t magData[3];
+	int32_t accData[3];
+	double compData[3];
+	BarometerData barometerData;
+	MagnetometerData magnetometerData;
+	AccelerometerData accelerometerData;
+	CompassData compassData;
+
+	switch (urt) {
+		case FLOOD_FRAME:
+			dataLength = sizeof(FLOOD_TAIL);
+			realloc(data,dataLength);
+			memcpy(data,FLOOD_TAIL,dataLength);
+			break;
+		case COMPASS_FRAME:
+			compassData = getCompassData();
+			compData[0] = compassData.pitch;
+			compData[1] = compassData.roll;
+			compData[2] = compassData.yaw;
+			dataLength = sizeof(compData);
+			realloc(data,dataLength);
+			memcpy(data,compData,dataLength);
+			break;
+		case BAROMETER_FRAME:
+			barometerData = getBarometerData();
+			dataLength = sizeof(barometerData.pressure);
+			realloc(data,dataLength);
+			memcpy(data,&barometerData.pressure,dataLength);
+			break;
+		case MAGNETOMETER_FRAME:
+			magnetometerData = getMagnetometerData();
+			magData[0] = magnetometerData.xValue;
+			magData[1] = magnetometerData.yValue;
+			magData[2] = magnetometerData.zValue;
+			dataLength = sizeof(magData);
+			realloc(data,dataLength);
+			memcpy(data,magData,dataLength);
+			break;
+		case ACCELEROMETER_FRAME:
+			accelerometerData = getAccelerometerData();
+			accData[0] = accelerometerData.xValue;
+			accData[1] = accelerometerData.yValue;
+			accData[2] = accelerometerData.zValue;
+			dataLength = sizeof(accData);
+			realloc(data,dataLength);
+			memcpy(data,accData,dataLength);
+			break;
+		case TEMPERATURE_FRAME:
+			barometerData = getBarometerData();
+			dataLength = sizeof(barometerData.temperature);
+			realloc(data,dataLength);
+			memcpy(data,&barometerData.temperature,dataLength);
+			break;
+		default:
+			break;
+	}
+	tail.data = data;
+	tail.dataLength = dataLength;
+	return tail;
+}
+
+static UnsolicitedResponseTail mergeTails(UnsolicitedResponseTail firstTail,UnsolicitedResponseTail secondTail) {
+	UnsolicitedResponseTail tempTail;
+	// memory allocated with malloc never loses scope until it is forcefully freed
+	uint8_t* tempData = malloc((firstTail.dataLength+1+secondTail.dataLength));
+	memcpy(tempData,firstTail.data,firstTail.dataLength);
+	memcpy(tempData+firstTail.dataLength,&TAIL_SEPARATOR,1);
+	memcpy(tempData+firstTail.dataLength+1,secondTail.data,secondTail.dataLength);
+	free(firstTail.data);
+	free(secondTail.data);
+	tempTail.data = tempData;
+	tempTail.dataLength = firstTail.dataLength+1+secondTail.dataLength;
+	return tempTail;
+}
+
+int64_t twosComplementToSignedInteger(uint32_t rawValue,SignBitIndex sbi) {
+	switch (sbi) {
+		case TWOS_COMPLEMENT_24_BIT:
+		{
+			if ((rawValue & 0x00800000) == 0)
+				return (rawValue & 0x00FFFFFF);
+			return ((int64_t)(~((rawValue & 0x00FFFFFF) - 1))) * -1;
+		}
+		case TWOS_COMPLEMENT_16_BIT:
+		{
+			if ((rawValue & 0x00008000) == 0)
+				return (rawValue & 0x0000FFFF);
+			return ((int64_t)(~((rawValue & 0x0000FFFF) - 1))) * -1;
+		}
+		default:
+			return NAN;
+	}
+}
+
+static BarometerData getBarometerData() {
+	uint8_t rawSensorData[5];
+	BarometerData barometerData;
+	HAL_I2C_Mem_Read(&hi2c1,0xBA,0x28,I2C_MEMADD_SIZE_8BIT,rawSensorData,5,HAL_MAX_DELAY);
+
+	uint32_t pressureTwosComplement = (rawSensorData[2] << 16) | (rawSensorData[1] << 8) | rawSensorData[0];
+	int64_t pressureRaw = twosComplementToSignedInteger(pressureTwosComplement,TWOS_COMPLEMENT_24_BIT);
+	barometerData.pressure = (int32_t)(pressureRaw/4096);
+
+	uint32_t temperatureTwosComplement = (rawSensorData[4] << 8) | rawSensorData[3];
+	int64_t temperatureRaw = twosComplementToSignedInteger(temperatureTwosComplement,TWOS_COMPLEMENT_16_BIT);
+	barometerData.temperature = (int32_t)(temperatureRaw/100);
+
+	return barometerData;
+}
+
+static CompassData getCompassData() {
+	double temp[] = {0.0,0.0,0.0};
+	int32_t magData[] = {0,0,0};
+	int32_t accData[] = {0,0,0};
+	MagnetometerData magnetometerData;
+	AccelerometerData accelerometerData;
+	CompassData compassData;
+	double pitch = 0;
+	double roll = 0;
+	double yaw = 0;
+	double degrees = 0;
+
+	accelerometerData = getAccelerometerData();
+	accData[0] = accelerometerData.xValue;
+	accData[1] = accelerometerData.yValue;
+	accData[2] = accelerometerData.zValue;
+
+	magnetometerData = getMagnetometerData();
+	magData[0] = magnetometerData.xValue;
+	magData[1] = magnetometerData.yValue;
+	magData[2] = magnetometerData.zValue;
+
+	roll = atan2((float)accData[Y],accData[Z]); // phi
+	temp[0] = magData[Y] * cos(roll) - magData[Z] * sin(roll);
+	temp[1] = magData[Y] * sin(roll) + magData[Z] * cos(roll);
+	temp[2] = accData[Y] * sin(roll) + accData[Z] * cos(roll);
+	//pitch = atan((-1 * accData[X]) / temp[2]); // theta
+	pitch = atan2(accData[X] , temp[2]); // theta
+
+#ifndef NSERDEBUG
+	char message[255];
+	sprintf(message, "Pitch: %6lf\t Roll: %6lf\r\n", pitch, roll);
+	HAL_UART_Transmit(&huart2, (uint8_t *) message, strlen(message), HAL_MAX_DELAY);
+#endif
+
+	//Determining the compass heading
+	temp[2] = magData[X] * cos(pitch) + temp[1] * sin(pitch);
+	temp[1] = (-1 * magData[X]) * sin(pitch) + temp[1] * cos(pitch);
+	yaw = atan2(-1 * temp[0], temp[2]); // psi
+	degrees = yaw * 57.2958; // 180/PI = 57.2958
+
+#ifndef NSERDEBUG
+	//Obtaining wind direction
+	if(degrees > 360) degrees -= 360;
+	if(degrees < 0) degrees += 360;
+
+	sprintf(message, "Degrees: %6lf\r\n", degrees);
+	HAL_UART_Transmit(&huart2, (uint8_t *) message, strlen(message), HAL_MAX_DELAY);
+
+	if(degrees > 22.5 && degrees < 67.5)
+		sprintf(message, "Direction: North-East\r\n");
+	else if(degrees > 67.5 && degrees < 112.5)
+		sprintf(message, "Direction: East\r\n");
+	else if(degrees > 112.5 && degrees < 157.5)
+		sprintf(message, "Direction: South-East\r\n");
+	else if(degrees > 157.5 && degrees < 202.5)
+		sprintf(message, "Direction: South\r\n");
+	else if(degrees > 202.5 && degrees < 247.5)
+		sprintf(message, "Direction: South-West\r\n");
+	else if(degrees > 247.5 && degrees < 292.5)
+		sprintf(message, "Direction: West\r\n");
+	else if(degrees > 292.5 && degrees < 337.25)
+		sprintf(message, "Direction: North-West\r\n");
+	else if(degrees > 337.25 || degrees < 22.5)
+		sprintf(message, "Direction: North\r\n");
+
+	HAL_UART_Transmit(&huart2, (uint8_t *) message, strlen(message), HAL_MAX_DELAY);
+#endif
+
+	compassData.pitch = pitch;
+	compassData.roll = roll;
+	compassData.yaw = yaw;
+	return compassData;
+}
+
+static MagnetometerData getMagnetometerData() {
+	int32_t axes_m[3];
+	get_m_axes(axes_m);
+	MagnetometerData magnetometerData;
+	magnetometerData.xValue = axes_m[0] - hardIronFault[X];
+	magnetometerData.yValue = axes_m[1] - hardIronFault[Y];
+	magnetometerData.zValue = axes_m[2] - hardIronFault[Z];
+	return magnetometerData;
+}
+
+static AccelerometerData getAccelerometerData() {
+	int32_t axes_a[3];
+	get_a_axes(axes_a);
+	AccelerometerData accelerometerData;
+	accelerometerData.xValue = axes_a[0];
+	accelerometerData.yValue = axes_a[1];
+	accelerometerData.zValue = axes_a[2];
+	return accelerometerData;
+}
+
+static void get_m_axes(int32_t *pData) {
+	int16_t pDataRaw[3];
+
+	get_m_axes_raw(pDataRaw);
+
+	// multiply by 1.5 through addition and shift right
+	pData[0] = (int32_t) (pDataRaw[0] + (pDataRaw[0] >> 1));
+	pData[1] = (int32_t) (pDataRaw[1] + (pDataRaw[1] >> 1));
+	pData[2] = (int32_t) (pDataRaw[2] + (pDataRaw[2] >> 1));
+}
+
+static void get_m_axes_raw(int16_t *pData) {
+	uint8_t status[1];
+	uint8_t regValue[6] = {[0 ... 5] = 0};
+	int16_t *regValueInt16;
+
+	// turn on magnetometer
+	uint8_t REG_VALUE = LSM303_MAG_LPEN | LSM303_MAG_ODR_10HZ | LSM303_MAG_MD_CON;
+	HAL_I2C_Mem_Write(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_CFG_REGA,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+	while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_MAG_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+	do
+		HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_SREG,I2C_MEMADD_SIZE_8BIT,status,1,100);
+	while(!CHECK_BIT(*status,LSM303_MAG_XYZDA));
+
+	// turn off magnetometer
+	REG_VALUE = LSM303_MAG_LPEN | LSM303_MAG_ODR_10HZ | LSM303_MAG_MD_IDLE;
+	HAL_I2C_Mem_Write(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_CFG_REGA,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+	while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_MAG_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_X_L,I2C_MEMADD_SIZE_8BIT,&regValue[0],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_X_H,I2C_MEMADD_SIZE_8BIT,&regValue[1],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_Y_L,I2C_MEMADD_SIZE_8BIT,&regValue[2],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_Y_H,I2C_MEMADD_SIZE_8BIT,&regValue[3],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_Z_L,I2C_MEMADD_SIZE_8BIT,&regValue[4],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_MAG_ADDR,LSM303_MAG_Z_H,I2C_MEMADD_SIZE_8BIT,&regValue[5],1,100);
+
+	regValueInt16 = (int16_t *)regValue;
+
+	pData[0] = regValueInt16[0];
+	pData[1] = regValueInt16[1];
+	pData[2] = regValueInt16[2];
+}
+
+static void get_a_axes(int32_t *pData) {
+	int16_t pDataRaw[3];
+
+	get_a_axes_raw(pDataRaw);
+
+	pData[0] = (int32_t) (((pDataRaw[0] >> 8) * LSM303_ACC_SENSITIVITY[2][0] + 500) / 1000);
+	pData[1] = (int32_t) (((pDataRaw[1] >> 8) * LSM303_ACC_SENSITIVITY[2][0] + 500) / 1000);
+	pData[2] = (int32_t) (((pDataRaw[2] >> 8) * LSM303_ACC_SENSITIVITY[2][0] + 500) / 1000);
+}
+
+static void get_a_axes_raw(int16_t *pData) {
+	uint8_t status[1];
+	uint8_t regValue[6] = {[0 ... 5] = 0};
+	int16_t regValueInt16[3] = {[0 ... 2] = 0};
+
+	// turn on accelerometer
+	uint8_t REG_VALUE = LSM303_ACC_ODR_1HZ | LSM303_ACC_XYZEN | LSM303_ACC_LPEN;
+	HAL_I2C_Mem_Write(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_CTRL_REG1,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+	while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_ACC_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+	do
+		HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_SREG,I2C_MEMADD_SIZE_8BIT,status,1,100);
+	while(!CHECK_BIT(*status,LSM303_ACC_XYZDA));
+
+	// turn off accelerometer
+	REG_VALUE = LSM303_ACC_PWR_DWN | LSM303_ACC_XYZEN | LSM303_ACC_LPEN;
+	HAL_I2C_Mem_Write(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_CTRL_REG1,I2C_MEMADD_SIZE_8BIT,&REG_VALUE,1,HAL_MAX_DELAY);
+	while (HAL_I2C_IsDeviceReady(&hi2c1,LSM303_ACC_ADDR,1,HAL_MAX_DELAY) != HAL_OK);
+
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_X_L,I2C_MEMADD_SIZE_8BIT,&regValue[0],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_X_H,I2C_MEMADD_SIZE_8BIT,&regValue[1],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_Y_L,I2C_MEMADD_SIZE_8BIT,&regValue[2],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_Y_H,I2C_MEMADD_SIZE_8BIT,&regValue[3],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_Z_L,I2C_MEMADD_SIZE_8BIT,&regValue[4],1,100);
+	HAL_I2C_Mem_Read(&hi2c1,LSM303_ACC_ADDR,LSM303_ACC_Z_H,I2C_MEMADD_SIZE_8BIT,&regValue[5],1,100);
+
+	regValueInt16[0] = (regValue[1] << 8) | regValue[0];
+	regValueInt16[1] = (regValue[3] << 8) | regValue[2];
+	regValueInt16[2] = (regValue[5] << 8) | regValue[4];
+
+	pData[0] = regValueInt16[0];
+	pData[1] = regValueInt16[1];
+	pData[2] = regValueInt16[2];
+}
+
+static void enterStopMode(uint32_t regulator) {
+	HAL_SuspendTick();
+	__HAL_RCC_PWR_CLK_ENABLE();
+	HAL_PWR_EnterSTOPMode(regulator, PWR_SLEEPENTRY_WFI);
+	HAL_ResumeTick();
+}
+
+static void enterSleepMode(uint32_t regulator) {
+	HAL_SuspendTick();
+	__HAL_RCC_PWR_CLK_ENABLE();
+	HAL_PWR_EnterSLEEPMode(regulator, PWR_SLEEPENTRY_WFI);
+	HAL_ResumeTick();
+}
+
+void HAL_RTCEx_WakeUpTimerEventCallback(RTC_HandleTypeDef *hrtc) {
+	if(!TRANSMITTING) TRANSMITTING = 1;
+}
 /* USER CODE END 4 */
 
 /**
