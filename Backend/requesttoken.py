ca467023657f20277c79777424d7f26676ed2004
Ivan Herrera <ivandegrote@gmail.com>
2018-01-14 18:06:40 +0100

    Documented script code

--- Backend/requesttoken.py
+++ Backend/requesttoken.py
@@ -14,18 +14,11 @@ import jsonpickle
 import json
 
 
+# This script is designed to request a new API-token for our RPC calls, may the current one expire
+
 url = "http://thingsboard.idlab.uantwerpen.be:8080/api/auth/login"
 data = '{"username":"student@lopow-coaw-lab.uantwerpen.be", "password":"student"}'
 headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 r = requests.post(url, data=data, headers=headers)
 jwt_token = r.json()['token']
 print "Bearer "+ jwt_token
-token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdHVkZW50QGxvcG93LWNvYXctbGFiLnVhbnR3ZXJwZW4uYmUiLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sInVzZXJJZCI6IjZjYzYzZTQwLWNhY2MtMTFlNy05ZjFiLTg1ZTZkZDEwYTJlOCIsImVuYWJsZWQiOnRydWUsImlzUHVibGljIjpmYWxzZSwidGVuYW50SWQiOiJmNTliMDU4MC1iNTA5LTExZTctYWIxYS04NWU2ZGQxMGEyZTgiLCJjdXN0b21lcklkIjoiMTM4MTQwMDAtMWRkMi0xMWIyLTgwODAtODA4MDgwODA4MDgwIiwiaXNzIjoidGhpbmdzYm9hcmQuaW8iLCJpYXQiOjE1MTI1NTk3MTgsImV4cCI6MTUyMTU1OTcxOH0.LX6GFJjJynmC2egjLniHtihCwE69u-LdLKwKUuzZyDyA5_vlsIBMxJyfdeEdx1DjTR4ecw_r0hlFnqrUBFQpNg"
-rpc_url = "http://thingsboard.idlab.uantwerpen.be:8080/api/plugins/rpc/twoway/b3acfa00-b803-11e7-bebc-85e6dd10a2e8"
-rpc_headers = {'Content-type': 'application/json', 'Accept': '/', 'X-Authorization': token}
-cmd = Command.create_with_read_file_action_system_file(File(0x00,8),InterfaceType.HOST)
-rpc_data = {"method": "execute-alp-async", "params":  jsonpickle.encode(cmd), "timeout": 500}
-print rpc_data
-r = requests.post(rpc_url, data=json.dumps(rpc_data), headers=rpc_headers)
-print r.status_code
-print r.text
